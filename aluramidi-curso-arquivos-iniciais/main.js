4b2e9180b7c7defe43fc370b200163f90a141233
Bruno Souzaaa <106277678+Brunosouzaaa@users.noreply.github.com>
2023-09-22 08:19:18 -0300

    22/09/2023

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,5 +1,41 @@
 alert('dirceu');
 
+<<<<<<< HEAD
+function tocaospom() {document .querySelector('.tecla_Pom')};
+
+document .querySelector('.tecla_Pom') .onclick = tocaospom;
+
+function tocaospom() {document .querySelector('.tecla_Clap')};
+
+document .querySelector('.tecla_Clap') .onclick = tocaospom;
+
+function tocaospom() {document .querySelector('.tecla_tim')};
+
+document .querySelector('.tecla_tim') .onclick = tocaospom;
+
+function tocaospom() {document .querySelector('.tecla_puff')};
+
+document .querySelector('.tecla_puff') .onclick = tocaospom;
+
+function tocaospom() {document .querySelector('.tecla_splash')};
+
+document .querySelector('.tecla_splash') .onclick = tocaospom;
+
+function tocaospom() {document .querySelector('.tecla_toim')};
+
+document .querySelector('.tecla_toim') .onclick = tocaospom;
+
+function tocaospom() {document .querySelector('.tecla_psh')};
+
+document .querySelector('.tecla_psh') .onclick = tocaospom;
+
+function tocaospom() {document .querySelector('.tecla_tic')};
+
+document .querySelector('.tecla_tic') .onclick = tocaospom;
+
+function tocaospom() {document .querySelector('.tecla_tom')};
+
+document .querySelector('.tecla_tom') .onclick = tocaospom;
 function tocaospom() {document .querySelector('.tecla_pom')};
 
 document .querySelector('.tecla_pom') .onclick = tocaospom
